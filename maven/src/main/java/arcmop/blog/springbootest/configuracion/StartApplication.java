9f61c78088c9b4f3689d228ad36913a50d9d16ef
gabocuadros <gabrielcuadros@gmail.com>
2020-08-19 16:27:58 -0500

    need to fix

--- maven/src/main/java/arcmop/blog/springbootest/configuracion/StartApplication.java
+++ maven/src/main/java/arcmop/blog/springbootest/configuracion/StartApplication.java
@@ -2,8 +2,10 @@ package arcmop.blog.springbootest.configuracion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "arcmop.blog.springbootest")
+@SpringBootApplication(scanBasePackages = "arcmop.blog.springbootest",exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class StartApplication {
 
 	public static void main(String[] args) {
--- maven/src/main/java/arcmop/blog/springbootest/controladores/ControladorHolaMundo.java
+++ maven/src/main/java/arcmop/blog/springbootest/controladores/ControladorHolaMundo.java
@@ -1,72 +1,87 @@
 package arcmop.blog.springbootest.controladores;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.gte;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Value;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
 @RestController
 @RequestMapping
 public class ControladorHolaMundo {
     
-   @Value("${app.mongodb-server}")
-   private String mongodb_server;
+    public static MongoCollection<Document> collection;
+    public static MongoDatabase database;
+    public static MongoClient mongoClient;
+    
+    public static void main(String[] args) {
+        Logger.getLogger("org.mongodb.driver").setLevel(Level.ALL);
+        //String connectionString = System.getProperty("mongodb.uri");
+        //mongodb://admin:admin@10.108.71.135:27017/sumas?authSource=admin
+        //mvn compile exec:java -Dexec.mainClass="arcmop.blog.springbootest.configuracion.StartApplication"
        
-   @Value("${app.mongodb-port}")
-   private int mongodb_port;
-   
-   @Value("${app.mongodb-db}")
-   private String mongodb_database;
+    }
    
  
-    MongoClient mongoClient = new MongoClient(mongodb_server, mongodb_port);
-    DB database = mongoClient.getDB(mongodb_database);
-   
-
     @RequestMapping(value = "/sumar/{sum01}/{sum02}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     Map saludar(@PathVariable("sum01") Integer sum01, @PathVariable("sum02") Integer sum02) {
         
+        
+        
+        
+       
         Map resultadototal= new LinkedHashMap();
         resultadototal.put("sumando1", String.valueOf(sum01));
         resultadototal.put("sumando2", String.valueOf(sum02));
         resultadototal.put("resultado", String.valueOf(sum01 + sum02));
         System.out.println("suma realizada  "+sum01+"+ "+sum02+ " = "+String.valueOf(sum01 + sum02));
         
-       /* DBCollection collection = database.getCollection("sumas");
-        BasicDBObject document = new BasicDBObject();
-        document.put("sumando1", String.valueOf(sum01));
-        document.put("sumando2", String.valueOf(sum02));
-        document.put("resultado", String.valueOf(sum01 + sum02));
-        document.put("time", System.currentTimeMillis());
-
-        collection.insert(document);
-    */
+        Document sumainput = new Document("_id", new ObjectId());      
+        sumainput.put("sumando1", String.valueOf(sum01));
+        sumainput.put("sumando2", String.valueOf(sum02));
+        sumainput.put("resultado", String.valueOf(sum01 + sum02));
+        sumainput.put("time", System.currentTimeMillis());
+        try {
+            
+        mongoClient =  MongoClients.create("mongodb+srv://admin:admin@10.108.71.135:27017/sumas?authSource=admin"); 
+        database= mongoClient.getDatabase("sumas");
+        collection = database.getCollection("sumas");
+        collection.insertOne(sumainput);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return resultadototal;
     }
     
-     @RequestMapping(value = "/getall", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/getall", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     Map getall () {
          System.out.println("lista todos los componentes en la db");
          
-       /* BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("", "");
-        DBCollection collection = database.getCollection("sumas");
-        DBCursor cursor = collection.find(searchQuery);
- 
+        /*FindIterable<Document> iterable = collection.find(gte("resultado", "0"));        
+        MongoCursor<Document> cursor = iterable.iterator();
+        Map resultadototal= new LinkedHashMap();
+
         while (cursor.hasNext()) {
-            System.out.println(cursor.next());
+            resultadototal.put("value",cursor.next().toJson());
         }
 */
         return Collections.singletonMap("lista generica", "todos");
+        //return resultadototal;
     }
 
 }
